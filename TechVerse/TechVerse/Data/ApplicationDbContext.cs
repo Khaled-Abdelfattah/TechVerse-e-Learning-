cf3b7b8bb02fe80de7160778d78b7973d9934c90
Khaled Abdelfattah <khassaan779@gmail.com>
2025-12-07 15:10:47 +0200

    Updated the code

---
+++ TechVerse/TechVerse/Data/ApplicationDbContext.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Linq;
+using TechVerse.Models;
+namespace TechVerse.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Data Source=localhost\MSSQLSERVERS;Database=TechVerseDB;Integrated Security=True;TrustServerCertificate=True;");
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Announcement> Announcements { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Module> Modules { get; set; }
+        public DbSet<VideoModule> VideoModules { get; set; }
+        public DbSet<TextModule> TextModules { get; set; }
+
+        public DbSet<Exam> Exams { get; set; }
+        public DbSet<Submission> Submissions { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Enrollment>()
+                .HasOne(e => e.Student)
+                .WithMany(s => s.Enrollments)
+                .HasForeignKey(e => e.UserID)
+                .OnDelete(DeleteBehavior.Cascade); 
+
+            modelBuilder.Entity<Enrollment>()
+                .HasOne(e => e.Course)
+                .WithMany(c => c.Enrollments)
+                .HasForeignKey(e => e.CourseID)
+                .OnDelete(DeleteBehavior.Cascade); 
+
+            modelBuilder.Entity<Enrollment>()
+                .HasIndex(e => new { e.UserID, e.CourseID })
+                .IsUnique();
+
+            modelBuilder.Entity<Submission>()
+                .HasOne(s => s.Student)
+                .WithMany(st => st.Submissions)
+                .HasForeignKey(s => s.UserID);
+
+            modelBuilder.Entity<Submission>()
+                .HasOne(s => s.Exam)        
+                .WithMany()                
+                .HasForeignKey(s => s.ExamID)
+                .OnDelete(DeleteBehavior.Restrict); 
+
+            modelBuilder.Entity<Module>()
+                .HasOne(m => m.Course)
+                .WithMany(c => c.Modules)
+                .HasForeignKey(m => m.CourseID);
+
+
+            modelBuilder.Entity<Exam>()
+                .HasOne(e => e.Course)
+                .WithMany(c => c.Exams)
+                .HasForeignKey(e => e.CourseID);
+        }
+    }
+}
